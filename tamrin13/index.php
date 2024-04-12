6caf8585bced4f0c34482cc659e27cb4d9b2d311
mohammad <160034369+mohammadzazayry@users.noreply.github.com>
2024-04-12 08:13:46 -0700

    Add files via upload

---
+++ tamrin13/index.php
@@ -0,0 +1,37 @@
+<?php
+echo 'hello world from <b>php!</b> <br/>';
+echo "<h1>a</h1>";
+echo"<h1>b</h1>";
+echo"<h1>c</h1>";
+
+echo"<br/>";
+
+
+$fname ="mohammad";
+$lname ="zazayry";
+$age = 23;
+$job = 'مهندس';
+
+echo $fname;
+echo"<br/>";
+echo$age;
+echo"<br/>;
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+<mata charest="utf-8">
+<meta http-eqiv="x-ua-comatible" content=ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>document</title>
+</head>
+<body>
+<header>
+<h1><?php echo $fname; ?></h1>
+<p><?php echo $job; ?></p>
+</header>
+</body>
+</html>

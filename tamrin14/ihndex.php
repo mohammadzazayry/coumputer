ee183c3f22bb6a75153feebbe9a792d68911162b
mohammad <160034369+mohammadzazayry@users.noreply.github.com>
2024-04-16 12:53:19 -0700

    Add files via upload

---
+++ tamrin14/ihndex.php
@@ -0,0 +1,29 @@
+<?php
+
+//string
+$name = 'mohammad';
+$job  = 'injunior';
+$summary = 'i am mohammad';
+
+//integer
+$age = 23;
+
+// float
+$weight = 91.4;
+
+//boolean
+$ismale = true;
+// echo true;
+
+
+$str = "10";
+$int = "20";
+$sum = $str + $int;
+echo ($sum);
+
+$x = 7;
+$y = 4;
+
+echo $x+$y;
+echo "<br/>";
+

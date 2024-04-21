0a5ace7ebfd47cda3daa94c6b1b2b99fbaf02214
mohammad <160034369+mohammadzazayry@users.noreply.github.com>
2024-04-21 05:52:49 -0700

    Add files via upload

---
+++ tavg/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$scores=array(5,4,11,6,7);
+$avg = ($scores[0]+$scores[1]+$scores[2]+$scores[3]+$scores[4]+$scores[5]/5);
+echo"the average of your scoures is: $avg;
+$variance=array_variance($scores);
+varians:...
+>
